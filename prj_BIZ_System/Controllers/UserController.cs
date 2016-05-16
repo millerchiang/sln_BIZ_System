13c6550ef7c39f769491c6e55cdbc7003e3be6d9
miller <millerchiang@wavegis.com.tw>
2016-05-16 18:47:15 +0800

    [add][1][new]

--- prj_BIZ_System/Controllers/UserController.cs
+++ prj_BIZ_System/Controllers/UserController.cs
@@ -1,11 +1,7 @@
-﻿using IBatisNet.DataMapper;
-using prj_BIZ_System.Models;
+﻿using prj_BIZ_System.Models;
 using prj_BIZ_System.Services;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace prj_BIZ_System.Controllers
@@ -13,10 +9,108 @@ namespace prj_BIZ_System.Controllers
 
     public class UserController : Controller
     {
-        public ActionResult Register()
+
+        public UserService userService;
+
+        public UserController()
         {
+            userService = new UserService();
+        }
+
+        public ActionResult UserList()
+        {
+            IList<UserInfoModel> userInfoModels = userService.GetUserInfoList();
+            ViewData["list"] = userInfoModels;
             return View();
         }
-    }
 
+
+
+        public ActionResult UserInsert()
+        {
+
+            if (Request["user_id"] != null)
+            {
+                UserInfoModel model = new UserInfoModel();
+                model.user_id = Request["user_id"];
+                model.user_pw = Request["user_pw"];
+                model.enterprise_type = Request["enterprise_type"];
+                model.company = Request["company"];
+                //            model.endtime = DateTime.Parse(Request["endtime"]);
+                model.company_en = Request["company_en"];
+                model.leader = Request["leader"];
+                model.addr = Request["addr"];
+                model.leader_en = Request["leader_en"];
+                model.addr_en = Request["addr_en"];
+                model.contact = Request["contact"];
+                model.contact_en = Request["contact_en"];
+                model.phone = Request["phone"];
+                model.email = Request["email"];
+                model.capital = int.Parse(Request["capital"]);
+                model.revenue = Request["revenue"];
+                model.website = Request["website"];
+                model.info = Request["info"];
+                model.info_en = Request["info_en"];
+                userService.UserInfoInsertOne(model);
+            }
+            return Redirect("../Home/Index");
+//            return Redirect("UserList");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            UserInfoModel model = null;
+
+            if (Request["user_id"] != null)
+            {
+                string user_id = Request["user_id"];
+                model = userService.GeUserInfoOne(user_id);
+                ViewBag.user = model;
+            }
+
+            IList<EnterpriseSortModel> userSortModels = userService.GetSortList();
+            ViewData["sortlist"] = userSortModels;
+
+            return View(model);
+        }
+
+
+
+        public ActionResult DeleteUser()
+        {
+            UserInfoModel model = new UserInfoModel();
+            model.user_id = Request["user_id"];
+            userService.UserInfoDelectOne(model.user_id);
+            return Redirect("UserList");
+        }
+
+        [HttpPost]
+        public ActionResult UserUpdate(UserInfoModel model)
+        {
+
+//            model.user_id = Request["user_id"];
+            model.user_pw = Request["user_pw"];
+            model.enterprise_type = Request["enterprise_type"];
+            model.company = Request["company"];
+            //            model.endtime = DateTime.Parse(Request["endtime"]);
+            model.company_en = Request["company_en"];
+            model.leader = Request["leader"];
+            model.addr = Request["addr"];
+            model.leader_en = Request["leader_en"];
+            model.addr_en = Request["addr_en"];
+            model.contact = Request["contact"];
+            model.contact_en = Request["contact_en"];
+            model.phone = Request["phone"];
+            model.email = Request["email"];
+            model.capital = int.Parse(Request["capital"]);
+            model.revenue = Request["revenue"];
+            model.website = Request["website"];
+            model.info = Request["info"];
+            model.info_en = Request["info_en"];
+            model.update_time = DateTime.Now;
+            userService.UserInfoUpdateOne(model);
+            return Redirect("../Home/Index");
+        }
+    }
 }
---
+++ prj_BIZ_System/Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using prj_BIZ_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.Configuration;
+
+
+namespace prj_BIZ_System.Services
+{
+    public class UserService
+    {
+        private DomSqlMapBuilder builder;
+        private ISqlMapper mapper;
+
+        public UserService()
+        {
+            builder = new DomSqlMapBuilder();
+            mapper = builder.Configure("DBSource/Config/SqlMap.config");
+        }
+
+        /* insert方法*/
+        public void UserInfoInsertOne(UserInfoModel userInfoModel)
+        {
+            mapper.Insert("UserInfo.InsertOne", userInfoModel);
+        }
+
+        /* select方法*/
+        public IList<EnterpriseSortModel> GetSortList()
+        {
+            return mapper.QueryForList<EnterpriseSortModel>("UserInfo.SelectAll_sort", null);
+        }
+
+
+        public IList<UserInfoModel> GetUserInfoList()
+        {
+            return mapper.QueryForList<UserInfoModel>("UserInfo.SelectAll", null);
+        }
+
+        public UserInfoModel GeUserInfoOne(string user_id)
+        {
+            return (UserInfoModel)mapper.QueryForObject("UserInfo.SelectOne", user_id);
+        }
+
+        /* delect方法*/
+        public int UserInfoDelectOne(string user_id)
+        {
+            Object obj = mapper.Delete("UserInfo.DeleteOne", user_id);
+            return (int)obj;
+        }
+
+        /*update方法*/
+        public int UserInfoUpdateOne(UserInfoModel userInfoModel)
+        {
+            Object obj = mapper.Update("UserInfo.UpdateOne", userInfoModel);
+            return (int)obj;
+        }
+
+
+    }
+}
