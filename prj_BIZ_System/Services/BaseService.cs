86c962b39a089d944b5c01771cd53b2a7b3b327c
alan123 <alan@>
2016-05-17 14:23:40 +0800

    [Add][3][新增新聞資料存取]

---
+++ prj_BIZ_System/Services/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prj_BIZ_System.Services
+{
+    public class BaseService
+    {
+        public static ISqlMapper mapper;
+        public BaseService()
+        {
+            DomSqlMapBuilder builder = new DomSqlMapBuilder();
+            mapper = builder.Configure("DBSource/Config/SqlMap.config");
+        }
+    }
+}
