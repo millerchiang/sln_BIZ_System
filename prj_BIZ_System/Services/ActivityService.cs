03f4a46bc7d5011166480ea35798cd8c7d071ea0
miller <millerchiang@wavegis.com.tw>
2016-05-20 14:51:11 +0800

    [Update][3][整理XML]

--- prj_BIZ_System/Services/ActivityService.cs
+++ prj_BIZ_System/Services/ActivityService.cs
@@ -16,28 +16,28 @@ namespace prj_BIZ_System.Services
 
         public IList<ActivityInfoModel> GetActivityInfoList()
         {
-            return mapper.QueryForList<ActivityInfoModel>("ActivityInfo.SelectAll", null); 
+            return mapper.QueryForList<ActivityInfoModel>("ActivityInfo.SelectActivityAll", null); 
         }
 
         public ActivityInfoModel GetActivityInfoOne(int activity_id)
         {
-            return (ActivityInfoModel)mapper.QueryForObject("ActivityInfo.SelectOne", activity_id);
+            return (ActivityInfoModel)mapper.QueryForObject("ActivityInfo.SelectActivityOne", activity_id);
         }
 
         public void ActivityInfoInsertOne(ActivityInfoModel activityInfoModel)
         {
-            mapper.Insert("ActivityInfo.InsertOne", activityInfoModel);
+            mapper.Insert("ActivityInfo.InsertActivityOne", activityInfoModel);
         }
 
         public int ActivityInfoDelectOne(int activity_id)
         {
-             Object obj = mapper.Delete("ActivityInfo.DeleteOne", activity_id);
+             Object obj = mapper.Delete("ActivityInfo.DeleteActivityOne", activity_id);
             return (int)obj;
         }
 
         public int ActivityInfoUpdateOne(ActivityInfoModel activityInfoModel)
         {
-           Object obj =  mapper.Update("ActivityInfo.UpdateOne", activityInfoModel);
+           Object obj =  mapper.Update("ActivityInfo.UpdateActivityOne", activityInfoModel);
             return (int)obj;
         }
 
@@ -52,29 +52,29 @@ namespace prj_BIZ_System.Services
 
         public IList<NewsModel> GetNewsAll()
         {
-            return mapper.QueryForList<NewsModel>("News.NewsSelectAll", null);
+            return mapper.QueryForList<NewsModel>("ActivityInfo.SelectNewsAll", null);
         }
 
         public NewsModel GetNewsOne(int news_no)
         {
             NewsModel newsModel = new NewsModel();
             newsModel.news_no = news_no;
-            return (NewsModel)mapper.QueryForObject("News.NewsSelectOne", newsModel);
+            return (NewsModel)mapper.QueryForObject("ActivityInfo.SelectNewsOne", newsModel);
         }
 
         public void NewsInsertOne(NewsModel newsModel)
         {
-            mapper.Insert("News.NewsInsertOne", newsModel);
+            mapper.Insert("ActivityInfo.InsertNewsOne", newsModel);
         }
 
         public void NewsUpdateOne(NewsModel newsModel)
         {
-            mapper.Update("News.NewsUpdateOne", newsModel);
+            mapper.Update("ActivityInfo.UpdateNewsOne", newsModel);
         }
 
         public void NewsDeleteOne(int news_no)
         {
-            mapper.Delete("News.NewsDeleteOne", news_no);
+            mapper.Delete("ActivityInfo.DeleteNewsOne", news_no);
         }
 
 
