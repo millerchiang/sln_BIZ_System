d3cfbaf8494ef1705eaeb82a6b800cf4c83a7534
carlos <carlos@wavegis.com.tw>
2016-05-24 10:10:35 +0800

    Revert "Revert "[add][1][新聞的文字編輯器未完成]""
    This reverts commit d08122be7cb1f893e6c7581d6729ec6d5747cf81.

--- prj_BIZ_System/Controllers/ActivityController.cs
+++ prj_BIZ_System/Controllers/ActivityController.cs
@@ -173,6 +173,7 @@ namespace prj_BIZ_System.Controllers
             }
             return View(activityModel.news);
         }
+
         [HttpPost]
         public ActionResult EditNewsInfoInsertUpdate(NewsModel news)
         {
