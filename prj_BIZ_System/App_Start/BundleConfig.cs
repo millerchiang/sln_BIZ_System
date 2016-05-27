03fd0b45ac88bc516eb7d2836523fbe335e9b773
carlos <carlos@wavegis.com.tw>
2016-05-27 17:00:42 +0800

    [update][3][推播管理]

--- prj_BIZ_System/App_Start/BundleConfig.cs
+++ prj_BIZ_System/App_Start/BundleConfig.cs
@@ -21,10 +21,11 @@ namespace prj_BIZ_System
                         "~/Scripts/jqueryUI/jquery-ui.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/datetimepicker").Include(
-                        "~/Scripts/datetimepicker/jquery-ui-timepicker-zh-TW.js",
                         "~/Scripts/datetimepicker/jquery-ui-sliderAccess.js",
                         "~/Scripts/datetimepicker/jquery-ui-timepicker-addon.js",
-                        "~/Scripts/datetimepicker/datepicker-zh-TW.js"));
+                        "~/Scripts/datetimepicker/datepicker-zh-TW.js",
+                        "~/Scripts/datetimepicker/jquery-ui-timepicker-zh-TW.js"
+                        ));
             
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
