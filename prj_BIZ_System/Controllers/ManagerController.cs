0db895ae451de221406a2a3644b490fe2c17d803
carlos <carlos@wavegis.com.tw>
2016-06-03 10:51:01 +0800

    [add][5][後台帳號管理]

---
+++ prj_BIZ_System/Controllers/ManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prj_BIZ_System.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult ManagerAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ prj_BIZ_System/Services/ManagerService.cs
@@ -0,0 +1,81 @@
+﻿using prj_BIZ_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.Configuration;
+using System.Collections;
+
+namespace prj_BIZ_System.Services
+{
+    public class ManagerService : _BaseService
+    {
+        public IList<PushSampleModel> getPushSampleAll()
+        {
+            return mapper.QueryForList<PushSampleModel>("Push.SelectPushSample", null);
+        }
+
+        public void PushSampleInsertOne(PushSampleModel model)
+        {
+            var result = mapper.Insert("Push.InsertPushSample", model);
+            return ;
+        }
+
+        public bool PushSampleUpdateOne(PushSampleModel model)
+        {
+            return mapper.Update("Push.UpdatePushSample", model) > 0;
+        }
+
+        public bool PushSampleDeleteOne(int sample_id)
+        {
+            var param = new PushSampleModel { sample_id = sample_id };
+            return mapper.Delete("Push.DeletePushSample", param) > 0;
+        }
+
+        public IList<ActivityInfoModel> getActivityInfoListAfterNow()
+        {
+            //DateTime dt = new DateTime(2016, 5, 12);
+            DateTime dt = new DateTime(2016, 5, 1);
+            var param = new ActivityInfoModel { starttime = dt };
+            IList<ActivityInfoModel> result = mapper.QueryForList<ActivityInfoModel>("Push.SelectActivityAfterNow", param);
+            return result;
+        }
+
+        public int getPushListCountBySampleId(int sample_id)
+        {
+            var param = new PushListModel { sample_id = sample_id };
+            int userCount = (int)mapper.QueryForObject("Push.SelectPushListCountBySampleId", param );
+            return userCount ;
+        }
+
+        public IList<PushListModel> getPushListByCondition(string push_type , string push_name)
+        {
+            var param = new PushListModel { push_type = push_type, push_name = push_name };
+            return mapper.QueryForList<PushListModel>("Push.getPushListByCondition", param);
+        }
+
+        public PushListModel getPushListOne(int? push_id)
+        {
+            var param = new PushListModel { push_id = push_id };
+            return mapper.QueryForObject<PushListModel>("Push.SelectPushListOne", param);
+        }
+
+        public void PushListInsertOne(PushListModel model)
+        {
+            mapper.Insert("Push.InsertPushList", model);
+        }
+
+        public void PushListUpdateOne(PushListModel model)
+        {
+            mapper.Update("Push.UpdatePushList", model);
+        }
+
+        public bool DeletePushListOne(int? push_id)
+        {
+            var param = new PushListModel { push_id = push_id };
+            return mapper.Delete("Push.DeletePushList", param) > 0 ;
+        }
+    }
+}
