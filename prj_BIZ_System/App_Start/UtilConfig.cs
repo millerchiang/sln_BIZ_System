7b6a091b9bd805c18533e2b7cdb964227508edbf
carlos <carlos@wavegis.com.tw>
2016-06-16 16:32:31 +0800

    temp

--- prj_BIZ_System/App_Start/UtilConfig.cs
+++ prj_BIZ_System/App_Start/UtilConfig.cs
@@ -5,10 +5,30 @@ using System.Web;
 
 namespace prj_BIZ_System.App_Start
 {
-    public class UtilConfig
-    {
 
-        
+    public class UtilConfig<T>
+    {
+        public static int pageNum = 10;
+        public PageList<T> dataPages(List<T> modelList , int current_page , string querystring)
+        {
+            PageList<T> page = new PageList<T>();
+            page.datalist = modelList.Skip<T>((current_page - 1) * pageNum).Take<T>(pageNum).ToList<T>();
+            page.pageNum = pageNum;
+            page.currentPage = current_page;
+            page.maxCount = modelList.Count();
+            page.maxPage = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(modelList.Count()) / pageNum));
+            return page;
+        }
 
     }
+
+    public class PageList<T>
+    {
+        public int currentPage { get; set; }
+        public int pageNum { get; set; }
+        public int maxCount { get; set; }
+        public int maxPage { get; set; }
+        public List<T> datalist { get; set; }
+    }
+
 }
