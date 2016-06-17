06f1583d4f304de14930bad37fdf1b9de04b7fc8
carlos <carlos@wavegis.com.tw>
2016-06-17 17:58:27 +0800

    [change][3][刪重複的定義]

--- prj_BIZ_System/Models/MatchModel.cs
+++ prj_BIZ_System/Models/MatchModel.cs
@@ -37,13 +37,4 @@ namespace prj_BIZ_System.Models
         public DateTime time_end { get; set; }    /*時間迄*/
     }
 
-    public class SchedulePeriodSetModel
-    {
-        public int period_sn { get; set; }        /*時段流水號*/
-        public int activity_id { get; set; }      /*活動編號*/
-        public DateTime time_start { get; set; }  /*時間起*/
-        public DateTime time_end { get; set; }    /*時間迄*/
-    }
-
-
 }
