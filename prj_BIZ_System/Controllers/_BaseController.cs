9cd5f6f56c5709660db9752a7428362d00a859cd
carlos <carlos@wavegis.com.tw>
2016-06-30 16:10:29 +0800

    [fix][3][匯入bug]

---
+++ prj_BIZ_System/Controllers/_BaseController.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prj_BIZ_System.Controllers
+{
+    public class _BaseController : Controller
+    {
+        public Logger logger = LogManager.GetCurrentClassLogger();
+    }
+}
