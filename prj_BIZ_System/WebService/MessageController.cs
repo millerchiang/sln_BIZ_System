03f2676dbb59f687c17abcf8d773dbe31a1d9c67
rong <s5952360@gmail.com>
2016-07-19 09:08:24 +0800

    [change][3][私訊列表(手機)]

--- prj_BIZ_System/WebService/MessageController.cs
+++ prj_BIZ_System/WebService/MessageController.cs
@@ -7,15 +7,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiContrib.ModelBinders;
 
 namespace prj_BIZ_System.WebService
 {
+    [MvcStyleBinding]
     public class MessageController : ApiController
     {
         private MessageService messageService = new MessageService();
 
-        [HttpPost]
-        public IList<MsgPrivate> getMessagePrivateList(string user_id, string date)
+        [HttpGet]
+        public IList<MsgPrivate> GetMessagePrivateList(string user_id, string date)
         {
             if (user_id == null || date == null) return null;
 
@@ -27,7 +29,7 @@ namespace prj_BIZ_System.WebService
                     msg_no = msgPrivateModel.msg_no,
                     msg_title = msgPrivateModel.msg_title,
                     company = msgPrivateModel.company,
-                    create_time = msgPrivateModel.create_time.ToString("yyyy-MM-dd HH:mm:ss")
+                    create_time = msgPrivateModel.create_time.ToString("yyyy-MM-dd HH:mm:ss:fff")
                 }
             ).ToList();
             return msgPrivates;
