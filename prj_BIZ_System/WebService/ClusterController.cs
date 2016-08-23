67c1f4123c18d4a109c2727bb6433733daffe986
rong <s5952360@gmail.com>
2016-08-23 17:53:07 +0800

    [added][3][Web api Cluster]

---
+++ prj_BIZ_System/WebService/ClusterController.cs
@@ -0,0 +1,24 @@
+﻿using prj_BIZ_System.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiContrib.ModelBinders;
+
+namespace prj_BIZ_System.WebService
+{
+    [MvcStyleBinding]
+    public class ClusterController : ApiController
+    {
+        private ClusterService clusterService = new ClusterService();
+
+        //[HttpGet]
+        //public IList<MsgPrivate> GetMessagePrivateList(string user_id)
+        //{
+        //    clusterService.GetClusterInfoListkw(user_id);
+        //    return msgPrivates;
+        //}
+    }
+}
